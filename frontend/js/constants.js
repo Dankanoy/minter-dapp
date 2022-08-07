6019314635ad58bd41d222f948317143f74b0a63
heider <heiderazaresceo@gmail.com>
2022-08-07 17:34:19 +0800

    rinkybe test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0x1047d2A1c35873F28fAba60B13a600b83cAA7F79"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Pixel Avatar NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
